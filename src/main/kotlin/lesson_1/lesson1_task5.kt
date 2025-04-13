6e96822e8716178aaeff8367fd7a0e6380b09211
Marat2626 <161500288+Marat2626@users.noreply.github.com>
2025-04-13 22:56:49 +0300

    KS-1-5

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_1
 
-fun main(){
+fun main() {
 
     val totalSeconds: Int = 4000
 
     val hours = totalSeconds / SECONDS_IN_HOUR
-    val minute = (totalSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val minutes = (totalSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
     val seconds = (totalSeconds % SECONDS_IN_HOUR) % SECONDS_IN_MINUTE
 
-    println(String.format("%02d:%02d:%02d", hours,minute,seconds))
+    println("%02d:%02d:%02d".format( hours, minutes, seconds))
 }
 const val SECONDS_IN_MINUTE = 60
 const val SECONDS_IN_HOUR = 3600
