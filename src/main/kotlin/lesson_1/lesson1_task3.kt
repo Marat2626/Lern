be386983a51f17a2dbb60625eb8a1d3867349ec9
Marat2626 <161500288+Marat2626@users.noreply.github.com>
2025-04-13 22:41:28 +0300

    KS-1-3

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -6,11 +6,11 @@ fun main() {
     var minute: Int = 7
 
     println("Год полета $year")
-    println("Вылет в " + String.format("%02d:%02d", hour, minute))
+    println("Вылет в  ${ String.format("%02d:%02d", hour, minute)}")
 // вывод с нулем посмотрел в интернете
     hour = 10
     minute  = 55
 
-    println("Прилет в " + String.format("%02d:%02d", hour, minute))
+    println("Прилет в  ${String.format("%02d:%02d", hour, minute)}")
 
 }
