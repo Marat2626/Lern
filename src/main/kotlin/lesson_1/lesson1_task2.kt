bbf839ffd280570d9fb9b098a1482dbd70f4a431
Marat2626 <161500288+Marat2626@users.noreply.github.com>
2025-04-13 22:37:53 +0300

    KS-1-2

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_1
+
 fun main() {
     val numberOrder: Int = 75
-    val PurchaseCompleted: String = "Спасибо за покупку "
+    val purchaseCompleted: String = "Спасибо за покупку "
 
-    println("Колтчество заказов $numberOrder $PurchaseCompleted")
+    println("Колтчество заказов $numberOrder $purchaseCompleted")
 
-    var personal: Int = 2000
+    var numberOfWorkers: Int = 2000
 
 //    println(personal)
 
-    personal = 1999
-    println(personal)
+    numberOfWorkers = 1999
+    println(numberOfWorkers)
 }
