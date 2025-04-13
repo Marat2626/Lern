7c90e4741c5670b42b8d5cceeae36e9acd6208ee
Marat2626 <161500288+Marat2626@users.noreply.github.com>
2025-04-13 17:02:39 +0300

    KS-1-2

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+fun main() {
+    val numberOrder: Int = 75
+    val PurchaseCompleted: String = "Спасибо за покупку "
+
+    println("Колтчество заказов $numberOrder $PurchaseCompleted")
+
+    var personal: Int = 2000
+
+//    println(personal)
+
+    personal = 1999
+    println(personal)
+}
