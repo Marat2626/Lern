e0089718ddfc34944201fbff455b2f061e22a4c2
Marat2626 <161500288+Marat2626@users.noreply.github.com>
2025-04-14 11:53:40 +0300

    KS-2-1

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main() {
+    val pointMarat = 3
+    val pointLera = 5
+    val pointMax = 3
+    val pointMasha = 4
+
+    val averagePoint = (pointMasha + pointMax + pointLera + pointMarat) / NUMBER_OF_STUDENTS
+
+    println("Средний балл учеников $averagePoint")
+
+}
+const val NUMBER_OF_STUDENTS = 4.0
