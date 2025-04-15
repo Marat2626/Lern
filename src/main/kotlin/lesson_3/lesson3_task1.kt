585c8dcc4a85c1e76d1a605d6feb384b42a88d41
Marat2626 <161500288+Marat2626@users.noreply.github.com>
2025-04-15 16:28:28 +0300

    KS-3-1

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var hellow = "Добрый день "
+    val name = "Марат"
+
+    println("$hellow $name")
+
+    hellow ="Добрый вечер "
+    println("$hellow $name")
+}
