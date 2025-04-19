c68c5a687168aa56a1dcd9dc5d6778bd528fb2a3
Marat2626 <161500288+Marat2626@users.noreply.github.com>
2025-04-19 10:58:22 +0300

    KS-4-4 ПЕРЕДЕЛАЛ

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,9 +3,10 @@ package org.example.lesson_4
 fun main() {
 
     val day = 5
+    val  isEven = (day % 2 == 0)
 
-    val armsAndAbs = (day % 2 != 0)
-    val legsAndBack = (day % 2 == 0)
+    val armsAndAbs = !isEven
+    val legsAndBack = isEven
 
     println("""
         Упражнение для рук $armsAndAbs
