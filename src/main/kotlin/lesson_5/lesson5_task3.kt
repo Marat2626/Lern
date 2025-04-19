cd464fd94a764884174c7ba1b391de0e16d08d99
Marat2626 <161500288+Marat2626@users.noreply.github.com>
2025-04-20 01:22:35 +0300

    KS-5-3

---
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -0,0 +1,29 @@
+
+package org.example.lesson_5
+
+fun main() {
+
+    val numberOne = 2
+    val numberTwo = 20
+
+    val input = readln().toInt()
+    val inputTwo = readln().toInt()
+
+    if (input or inputTwo in 0..42){
+
+        if ((input == numberOne && inputTwo == numberTwo) || (input == numberTwo && inputTwo == numberOne)) {
+            println("выйграл")
+        }
+        else if (((input == numberOne || input == numberTwo) && (inputTwo !== numberTwo && inputTwo !== numberOne)) || ((input !== numberOne && input !== numberTwo) && (inputTwo == numberOne || inputTwo == numberTwo))){
+            println("утешиетльный")
+        }
+        else if ((input !== numberOne && input !== numberTwo) || (inputTwo !== numberOne && inputTwo !== numberTwo)) {
+
+            println("Не угадал")
+        }
+    }
+    else {
+        println("Число должно быть от 0 до 42")
+    }
+
+}
