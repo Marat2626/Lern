9bd2f3df3bf10033657612fe51f48afbe645e25b
Marat2626 <161500288+Marat2626@users.noreply.github.com>
2025-04-23 11:14:01 +0300

    KS-6-1

--- src/main/kotlin/lesson_6/lesson6_1.kt
+++ src/main/kotlin/lesson_6/lesson6_1.kt
@@ -4,13 +4,14 @@ fun main() {
 
     val myNum = 32
 
+    println("Угодай мое число")
     try {
         do {
             val num = readln().toInt()
 
             when {
-                num > myNum -> println("Меньше")
-                num < myNum -> println("Больше")
+                num > myNum -> println("У меня меньше число")
+                num < myNum -> println("У меня больше число")
                 else -> println("Угадал")
             }
 
